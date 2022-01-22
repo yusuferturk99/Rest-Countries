807e88acd1eb7c349cea0cb9b1597cd5fe51250c
Yusuf <yusuf999@msn.com>
2022-01-22 12:37:30 +0300

    Tablonun rengini değiştirdim.

--- RestCountries/src/App.vue
+++ RestCountries/src/App.vue
@@ -1,51 +1,62 @@
 <template>
   <div id="app">
-    <div>
-      <b-form-input
-          placeholder="Tüm değerlerde ara"
-          id="bigbar"
-          v-model="searchText"
-      ></b-form-input>
-      <div>
-        <b-dropdown id="options" text="Filtre">
-          <b-dropdown-item v-b-toggle:my-button>name</b-dropdown-item>
-          <b-dropdown-item v-b-toggle:my-button1>capital</b-dropdown-item>
-          <b-dropdown-item v-b-toggle:my-button2>region</b-dropdown-item>
-          <b-dropdown-item v-b-toggle:my-button3>flag</b-dropdown-item>
-        </b-dropdown>
-      </div>
+    <div style="font-family: Calibri; color: black;">
+      <h1>List of Countries</h1>
+      <p style="font-size: large">Hello visitor, welcome to our website! Here, you can search for countries from all around the world. <br> So, what are you waiting for? Start searching! </p>
+      <p>Note: You can search for countries with the parameters as well.</p>
     </div>
+    <div style="background: #eff3ee;"><br></div>
+    <div style="background: #eff3ee;">
+      <div>
+        <b-form-input
+            placeholder="Search in all parameters"
+            id="bigbar"
+            v-model="searchText"
+        ></b-form-input>
+        <div>
+          <b-dropdown id="options" text="Filter">
+            <b-dropdown-item v-b-toggle:my-button>name</b-dropdown-item>
+            <b-dropdown-item v-b-toggle:my-button1>capital</b-dropdown-item>
+            <b-dropdown-item v-b-toggle:my-button2>region</b-dropdown-item>
+            <b-dropdown-item v-b-toggle:my-button3>flag</b-dropdown-item>
+          </b-dropdown>
+        </div>
+      </div>
 
-    
-    <div>
-      <b-collapse id="my-button">
-      <b-form-input  style="width: 47%; float: left; margin-right: 25px" placeholder="name" v-model="searchText1"></b-form-input>
-      </b-collapse>
-      <b-collapse id="my-button1">
-      <b-form-input style="width: 24%; float: left; margin-right: 25px" placeholder="capital" v-model="searchText2"></b-form-input>
-      </b-collapse>
-      <b-collapse id="my-button2">
-      <b-form-input style="width: 12%; float: left; margin-right: 25px" placeholder="region" v-model="searchText3"></b-form-input>
-      </b-collapse>
-      <b-collapse id="my-button3">
-      <b-form-input style="width: 10%; float: left; margin-right: 25px" placeholder="flag" v-model="searchText4"></b-form-input>
-      </b-collapse>
+      <div style="margin-bottom: 60px;">
+        <b-collapse id="my-button">
+          <b-form-input  style="width: 47%; float: left; margin-right: 25px;" placeholder="name" v-model="searchText1"></b-form-input>
+        </b-collapse>
+        <b-collapse id="my-button1">
+          <b-form-input style="width: 24%; float: left; margin-right: 25px" placeholder="capital" v-model="searchText2"></b-form-input>
+        </b-collapse>
+        <b-collapse id="my-button2">
+          <b-form-input style="width: 12%; float: left; margin-right: 25px" placeholder="region" v-model="searchText3"></b-form-input>
+        </b-collapse>
+        <b-collapse id="my-button3">
+          <b-form-input style="width: 10%; float: left; margin-right: 25px" placeholder="flag" v-model="searchText4"></b-form-input>
+        </b-collapse>
 
+      </div>
+
+      <div style="background: white;"><br></div>
     </div>
 
     <div class="text-center justify-content-between mx-auto" v-if="isLoading" style="width: 15rem; height: 15rem;">
       <b-spinner
-        v-for="variant in variants"
-        :variant="variant"
-        :key="variant"
+          v-for="variant in variants"
+          :variant="variant"
+          :key="variant"
       ></b-spinner>
     </div>
+
     <b-table striped hover :items="filteredData" :fields="fields" v-else id="table" >
 
       <template #cell(flag)="data">
         <img :src="data.value" style="width: 70px" />
       </template>
     </b-table>
+
   </div>
 </template>
 
@@ -79,28 +90,28 @@ export default {
       if (this.searchText !== "") {
         text = this.searchText
         return this.items.filter((x) =>
-          x.name.toLowerCase().includes(text.toLowerCase())
-      )
+            x.name.toLowerCase().includes(text.toLowerCase())
+        )
       } else if (this.searchText1 !== "") {
         text = this.searchText1
         return this.items.filter((x) =>
-          x.name.toLowerCase().includes(text.toLowerCase())
-      )
+            x.name.toLowerCase().includes(text.toLowerCase())
+        )
       } else if (this.searchText2 !== "") {
         text = this.searchText2
         return this.items.filter((x) =>
-          (x.capital + "").toLowerCase().includes(text.toLowerCase())
-      )
+            (x.capital + "").toLowerCase().includes(text.toLowerCase())
+        )
       } else if (this.searchText3 !== "") {
         text = this.searchText3
         return this.items.filter((x) =>
-          x.region.toLowerCase().includes(text.toLowerCase())
-      )
+            x.region.toLowerCase().includes(text.toLowerCase())
+        )
       } else {
         text = this.searchText4
         return this.items.filter((x) =>
-          x.flag.toLowerCase().includes(text.toLowerCase())
-      )
+            x.flag.toLowerCase().includes(text.toLowerCase())
+        )
       }
     }
   },
@@ -137,11 +148,12 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e49;
+  color: #2c3e50;
   margin: 10px;
 }
 #table {
   text-align: left;
+  background: #e0e6eb;
 }
 #bigbar {
   margin-bottom: 15px;
